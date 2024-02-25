e7633b0fe2e9b108e7c1651aeb1a9510631d0277
Misha <mishagrigoryan@gmail.com>
2024-02-24 23:46:18 -0500

    crimeType

--- explore.py
+++ explore.py
@@ -1,2 +1,17 @@
 import pandas as pd
 df = pd.read_csv('Booking Records - BookingDataAnon.csv')
+
+crimeTypes_nun = df['PRIMARY_CHARGE'].nunique()
+crimeTypes_un = df['PRIMARY_CHARGE'].unique()
+
+
+print(crimeTypes_nun)
+print(crimeTypes_un)
+
+import pandas as pd
+import numpy as np
+
+
+df = pd.DataFrame(crimeTypes_un)
+
+df.to_csv('crimeTypes_un.csv', index=False)  
