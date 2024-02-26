e59af099cfbc9a9b57cbe111b0dea351571a4a9b
Misha <mishagrigoryan@gmail.com>
2024-02-25 23:33:37 -0500

    grouped

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ df['day'] = df['DATE'].dt.day_name()
 df['month'] =df['DATE'].dt.month_name()
 df['year'] = df['DATE'].dt.year
 
-print(df.head(100))
+#print(df.head(100))
 
+grouped = df.groupby(['year','month','day'])
+
+result = grouped.size().reset_index(name='counts')
+print(result)
 # df['DATE'] = pd.to_datetime(df['DATE'], format='%m/%d/%Y')
 
 # crime_counts_by_zip_and_time = df.groupby([df['ZIP_CODE'], df['DATE'].dt.to_period("M")]).size().reset_index(name='Count')
