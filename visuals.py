f87436f6b4e7f83aca76095d677e4a7dee373c37
Misha <mishagrigoryan@gmail.com>
2024-02-26 23:58:21 -0500

    new df

--- visuals.py
+++ visuals.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('Booking Records - BookingDataAnon.csv')
+df['DATE'] = pd.to_datetime(df['DATE'])
+df['day'] = df['DATE'].dt.day_name()
+df['month'] =df['DATE'].dt.month_name()
+df['year'] = df['DATE'].dt.year
