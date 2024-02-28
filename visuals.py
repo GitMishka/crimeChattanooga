7c9fd84b7cb1c65cd30d4110edfe713569b194b8
Misha <mishagrigoryan@gmail.com>
2024-02-27 23:35:38 -0500

    visuals.py

--- visuals.py
+++ visuals.py
@@ -5,3 +5,5 @@ df['DATE'] = pd.to_datetime(df['DATE'])
 df['day'] = df['DATE'].dt.day_name()
 df['month'] =df['DATE'].dt.month_name()
 df['year'] = df['DATE'].dt.year
+
+#df.head(10).to_csv('visualsSample.csv')
